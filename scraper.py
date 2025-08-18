59c40be958850f85b90740d27122e007c54206d6
Hendrix <afamorim16@sapo.pt>
2025-08-19 00:15:31 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,14 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
+    "Accept-Language": "en-US,en;q=0.9,pt;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+}
 
 def get_prices(search_url: str):
     response = requests.get(search_url, headers=HEADERS, timeout=20)
